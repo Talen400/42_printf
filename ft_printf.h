15e76b76e10cb97d71e249dfaaa166ffb45cdc80
Talen Lavareda Pinto <TalenGitHub0@gmail.com>
2025-09-03 00:34:51 -0300

    v1.0.9

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,13 @@
 /*   By: tlavared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 15:52:43 by tlavared          #+#    #+#             */
-/*   Updated: 2025/08/22 21:05:19 by tlavared         ###   ########.fr       */
+/*   Updated: 2025/09/03 00:33:45 by tlavared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-// Para testes - remover depois
 # include "libft/libft.h"
 # include <stdarg.h>
 
